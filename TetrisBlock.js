7328acaa99b371618aadf360e9bd3616d6edcda7
innonion <innonion@gmail.com>
2019-05-06 20:46:37 +0800

    Multi-Combination Implemented

--- TetrisBlock.js
+++ TetrisBlock.js
@@ -57,7 +57,32 @@ var Option6 = [ [1, 0, 0, 0 ],
 TetrisBlocksOption.push(Option6);
 
 
-function getLength(Option)
+var Option7 =  [ [1, 1, 1, 1 ],
+                 [1, 1, 1, 1 ],
+                 [1, 0, 0, 1 ],
+                 [1, 1, 1, 1 ],
+                ];
+//TetrisBlocksOption.push(Option7);
+
+
+
+function getColumnLength(Option)
+{
+    var LongestLength = 0;
+    for ( var i = 0; i < 4; i++ )
+    {
+        var ColLength = 0;
+        for ( var j = 0; j <4; j++ )
+        {
+            if ( Option[i][j] == 1 ){ ColLength = j; }
+        }
+        if ( ColLength >= LongestLength ){ LongestLength = ColLength; }
+    }
+    return LongestLength + 1;
+}
+
+
+function getRowLength(Option)
 {
     var LongestLength = 0;
     for ( var i = 0; i < 4; i++ )
@@ -65,12 +90,19 @@ function getLength(Option)
         var RowLength = 0;
         for ( var j = 0; j <4; j++ )
         {
-            if ( Option[i][j] == 1 ){ RowLength++; }
+            if ( Option[i][j] == 1 ){ RowLength = i; }
         }
         if ( RowLength >= LongestLength ){ LongestLength = RowLength; }
     }
-    return LongestLength;
+    return LongestLength + 1;
 }
 
+
+
+function getRandomOption()
+{
+    var OptionValue = Math.floor( Math.random() * TetrisBlocksOption.length );
+    return TetrisBlocksOption[OptionValue];
+}
 
 
